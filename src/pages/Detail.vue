ad284186d4304914782542dc275d413078b11ab3
Len <jqlen0623@gmail.com>
2026-06-16 16:38:20 +0800

    feat: 路由_嵌套路由

---
+++ src/pages/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="news-list">
+    <li>標號: xxx</li>
+    <li>標題: xxx</li>
+    <li>內容: xxx</li>
+  </ul>
+</template>
+
+<script setup lang="ts" name="About"></script>
+
+<style scoped>
+.about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  color: rgb(85, 84, 84);
+  font-size: 18px;
+}
+</style>
