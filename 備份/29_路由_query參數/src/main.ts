36c88a9ca9081d2b8aaad40c44e00ab7355cf6b2
LenShi1989 <f0857gb@yahoo.com.tw>
2026-06-28 14:53:59 +0800

    feat: 路由_query參數

---
+++ 備份/29_路由_query參數/src/main.ts
@@ -0,0 +1,13 @@
+// 引入createApp用於創建應用
+import { createApp } from "vue";
+// 引入App根組件
+import App from "./App.vue";
+// 引入路由器
+import router from "./router/index.ts";
+
+// 創建一個應用
+const app = createApp(App);
+// 使用路由器
+app.use(router);
+// 掛載整個應用到app容器中
+app.mount("#app");
